3fee82d447dfe7df4928f302af6c8d268542a332
Daniele Cicala <10630561@polimi.it>
2025-06-08 11:27:43 +0200

    fix: age is now calculated based on current day

---
+++ calc_age.js
@@ -0,0 +1,19 @@
+function calAge() {
+    const ageRef = document.getElementById('age');
+  
+    const birthDate = new Date('1999-12-29');
+    const today = new Date();
+  
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const currentMonth = today.getMonth();
+    const currentDay = today.getDate();
+    const birthMonth = birthDate.getMonth();
+    const birthDay = birthDate.getDate();
+  
+    if (currentMonth < birthMonth || (currentMonth === birthMonth && currentDay < birthDay)) {
+      age--;
+    }
+  
+    ageRef.textContent = age;
+  }
+  
